b2a3c9300fbcd9abe7d0d9c80e006e987538b4cf
Lucas Gabriel <lucasfacul20222@gmail.com>
2023-09-27 22:51:49 -0300

    feat: Adicionado DAO de manipulação de arquivo das classes Reserva e Prazos

---
+++ src/dao/emprestimo/EmprestimoImplArquivo.java
@@ -0,0 +1,266 @@
+package dao.emprestimo;
+
+import dao.DAO;
+
+import model.Emprestimo;
+
+import utils.ArmazenamentoArquivo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * É responsável por armazenar todos os empréstimos do sistema em um arquivo binário, e estruturar os métodos
+ * necessários para inserir, consultar, alterar ou remover. Implementa a interface EmprestimoDAO.
+ *
+ * @author Lucas Gabriel.
+ */
+
+public class EmprestimoImplArquivo implements EmprestimoDAO{
+
+    private String nomeArquivoEmprestimoAtual;
+    private String nomeArquivoEmprestimoTotal;
+    private String nomePasta;
+    private List<Emprestimo> listaEmprestimoTotal;
+    private List<Emprestimo> listaEmprestimoAtual;
+
+    /**
+     * Construtor que atribui os nomes dos arquivos a serem resgatados e resgata a lista de armazenamento
+     * de emprestimos atuais e totais, armazenados em arquivo binário.
+     */
+
+    public EmprestimoImplArquivo() {
+        this.nomeArquivoEmprestimoAtual = "emprestimoAtual.dat";
+        this.nomeArquivoEmprestimoTotal = "emprestimoTotal.dat";
+        this.nomePasta = "Emprestimo";
+
+        this.listaEmprestimoTotal = ArmazenamentoArquivo.resgatar(this.nomeArquivoEmprestimoTotal,this.nomePasta);
+        this.listaEmprestimoAtual = ArmazenamentoArquivo.resgatar(this.nomeArquivoEmprestimoAtual,this.nomePasta);;
+    }
+
+    /**
+     * Método para adicionar um objeto do tipo Emprestimo na lista de armazenamento de empréstimos ativos
+     * e a de todos os empréstimo já feitos e guardar no arquivo.
+     *
+     * A disponibilidade do livro emprestado é alterada e a quantidade de empréstimo dele
+     * é somado 1.
+     *
+     * @param obj empréstimo que deve ser armazenado
+     * @return retorna objeto empréstimo criado
+     */
+
+    @Override
+    public Emprestimo criar(Emprestimo obj) {
+        DAO.getLivro().encontrarPorISBN(obj.getLivro()).setDisponivel(false);
+        DAO.getLivro().encontrarPorISBN(obj.getLivro()).setQtdEmprestimo(+1);
+
+        this.listaEmprestimoTotal.add(obj);
+        this.listaEmprestimoAtual.add(obj);
+        ArmazenamentoArquivo.guardar(this.listaEmprestimoTotal,this.nomeArquivoEmprestimoTotal,this.nomePasta);
+        ArmazenamentoArquivo.guardar(this.listaEmprestimoAtual,this.nomeArquivoEmprestimoAtual,this.nomePasta);
+
+        return obj;
+    }
+
+    /**
+     * Método para remover determinado Empréstimo da estrutura de armazenamento (lista e arquivo)
+     * de Empréstimos Ativos.
+     *
+     * @param id identificação do leitor que realizou o emprestimo a ser deletado
+     */
+
+    @Override
+    public void remover(int id) {
+        for(Emprestimo emp : this.listaEmprestimoAtual){
+            if(emp.getLeitor() == id){
+                this.listaEmprestimoAtual.remove(emp);
+                ArmazenamentoArquivo.guardar(this.listaEmprestimoAtual,this.nomeArquivoEmprestimoAtual,this.nomePasta);
+                return;
+            }
+        }
+    }
+
+    /**
+     * Deleta todos os objetos do tipo Emprestimo do banco de dados.
+     *
+     * Antes de deletar todos os empréstimos, a quantidade de empréstimos
+     * feitos de cada livro é resetado para 0.
+     * Os livros com empréstimos ativos tem sua disponibilidade alterada,
+     * visto que seus empréstimos serão deletados.
+     * Os limites de renovações dos leitores também são resetados para 0.
+     *
+     * É deletado os empréstimos ativos e todos os empréstimos já feitos.
+     */
+
+    @Override
+    public void removerTodos() {
+        for(Emprestimo emp : this.listaEmprestimoTotal){
+            DAO.getLivro().encontrarPorISBN(emp.getLivro()).setQtdEmprestimo(0);
+        }
+
+        for(Emprestimo emp : this.listaEmprestimoAtual){
+            DAO.getLivro().encontrarPorISBN(emp.getLivro()).setDisponivel(true);
+
+            if(DAO.getLeitor().encontrarPorId(emp.getLeitor()).getLimiteRenova()==1)
+                DAO.getLeitor().encontrarPorId(emp.getLeitor()).setLimiteRenova(0);
+        }
+
+        this.listaEmprestimoAtual.clear();
+        this.listaEmprestimoTotal.clear();
+        ArmazenamentoArquivo.guardar(this.listaEmprestimoTotal,this.nomeArquivoEmprestimoTotal,this.nomePasta);
+        ArmazenamentoArquivo.guardar(this.listaEmprestimoAtual,this.nomeArquivoEmprestimoAtual,this.nomePasta);
+    }
+
+    /**
+     * Método de retorno do Emprestimo através da busca por ID.
+     *
+     * @param id identificação do leitor que realizou o emprestimo a ser encontrado
+     * @return retorna objeto emprestimo encontrado
+     */
+
+    @Override
+    public Emprestimo encontrarPorId(int id) {
+        for(Emprestimo emp : this.listaEmprestimoAtual){
+            if(emp.getLeitor() == id)
+                return emp;
+        }
+
+        return null;
+    }
+
+    /**
+     * Método que encontra todos os empréstimos já feitos.
+     *
+     * @return retorna lista de todos os empréstimos já feitos encontrados
+     */
+
+    @Override
+    public List<Emprestimo> encontrarTodos() {
+        return this.listaEmprestimoTotal;
+    }
+
+    /**
+     * Método que encontra todos os empréstimos ativos.
+     *
+     * @return retorna lista de empréstimos ativos encontrados
+     */
+
+    @Override
+    public List<Emprestimo> encontrarTodosAtuais() {
+        return this.listaEmprestimoAtual;
+    }
+
+    /**
+     * Método que encontra todos os empréstimos já feitos por um determinado leitor.
+     *
+     * Objetos Emprestimo que possuam o ID informado, são adicionados numa
+     * lista e retornados.
+     *
+     * @param id identificação do leitor que realizou o emprestimo
+     * @return retorna lista de empréstimos de um leitor
+     */
+
+    @Override
+    public List<Emprestimo> encontrarHistoricoDeUmLeitor(int id) {
+        List<Emprestimo> historicoDoID = new ArrayList<Emprestimo>();
+
+        for(Emprestimo emp : this.listaEmprestimoTotal){
+            if(emp.getLeitor() == id)
+                historicoDoID.add(emp);
+        }
+
+        return historicoDoID;
+    }
+
+    /**
+     * Método que encontra todos os empréstimos já feitos de um determinado livro.
+     *
+     * Objetos Emprestimo que possuam o ISBN informado, são adicionados numa
+     * lista e retornados.
+     *
+     * @param isbn isbn do livro emprestado
+     * @return retorna lista de empréstimos de um livro
+     */
+
+    @Override
+    public List<Emprestimo> encontrarHistoricoDeUmLivro(double isbn) {
+        List<Emprestimo> historicoDoISBN = new ArrayList<Emprestimo>();
+
+        for(Emprestimo emp : this.listaEmprestimoTotal){
+            if(emp.getLivro() == isbn)
+                historicoDoISBN.add(emp);
+        }
+
+        return historicoDoISBN;
+    }
+
+    /**
+     * Método que encontra um empréstimo ativo pelo isbn do livro emprestado.
+     *
+     * @param isbn isbn do livro que foi emprestado
+     * @return retorna o empréstimo do livro encontrado
+     */
+
+    @Override
+    public Emprestimo encontrarPorISBN(double isbn) {
+        for(Emprestimo emp : this.listaEmprestimoAtual){
+            if(emp.getLivro() == isbn)
+                return emp;
+        }
+
+        return null;
+    }
+
+    /**
+     * Método que remove todos os empréstimos da estrutura de armazenamento (lista e arquivo),
+     * já feitos de um livro deletado do sistema.
+     *
+     * Esta forma de remoção em sequência, evita problemas de deslocamento de índice que podem
+     * ocorrer ao remover elementos de um ArrayList enquanto o percorremos.
+     *
+     * @param isbn isbn do livro deletado
+     */
+
+    @Override
+    public void removerTodosEmprestimosDeUmLivro(double isbn) {
+        List<Emprestimo> emprestimosARemover = new ArrayList<Emprestimo>();
+
+        for (Emprestimo emprestimo : this.listaEmprestimoTotal) {
+            if (emprestimo.getLivro() == isbn) {
+                emprestimosARemover.add(emprestimo);
+            }
+        }
+
+        this.listaEmprestimoTotal.removeAll(emprestimosARemover);
+        ArmazenamentoArquivo.guardar(this.listaEmprestimoTotal,this.nomeArquivoEmprestimoTotal,this.nomePasta);
+    }
+
+    /**
+     * Método que remove todos os empréstimos da estrutura de armazenamento (lista e arquivo),
+     * já feitos de um leitor deletado do sistema.
+     *
+     * Esta forma de remoção em sequência, evita problemas de deslocamento de índice que podem
+     * ocorrer ao remover elementos de um ArrayList enquanto o percorremos.
+     *
+     * @param id identificação do leitor deletado
+     */
+
+    @Override
+    public void removerTodosEmprestimoDeUmLeitor(int id) {
+        List<Emprestimo> emprestimosARemover = new ArrayList<Emprestimo>();
+
+        for (Emprestimo emprestimo : this.listaEmprestimoTotal) {
+            if (emprestimo.getLeitor() == id) {
+                emprestimosARemover.add(emprestimo);
+            }
+        }
+
+        this.listaEmprestimoTotal.removeAll(emprestimosARemover);
+        ArmazenamentoArquivo.guardar(this.listaEmprestimoTotal,this.nomeArquivoEmprestimoTotal,this.nomePasta);
+    }
+
+    @Override
+    public Emprestimo atualizar(Emprestimo obj) {
+        return null;
+    }
+}
---
+++ src/dao/reserva/ReservaImplArquivo.java
@@ -0,0 +1,269 @@
+package dao.reserva;
+
+import model.Reserva;
+
+import utils.ArmazenamentoArquivo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * É responsável por armazenar em um arquivo binário, todas as reservas de livros do sistema
+ * e estruturar os métodos necessários para inserir, consultar, alterar ou remover.
+ * Implementa a interface PrazosDAO.
+ *
+ * @author Lucas Gabriel.
+ */
+
+public class ReservaImplArquivo implements ReservaDAO{
+
+    private String nomeArquivo;
+    private String nomePasta;
+    private List<Reserva> listaReserva;
+
+    /**
+     * Construtor que atribui o nome do arquivo a ser resgatado e resgata a lista de armazenamento de reservas
+     * armazenadas em um arquivo binário.
+     */
+
+    public ReservaImplArquivo() {
+        this.nomeArquivo = "reserva.dat";
+        this.nomePasta = "Reserva";
+
+        this.listaReserva = ArmazenamentoArquivo.resgatar(this.nomeArquivo,this.nomePasta);
+    }
+
+    /**
+     * Método para adicionar um objeto do tipo Reserva na lista de armazenamento e guardar no arquivo.
+     *
+     * @param obj reserva que deve ser armazenada
+     * @return retorna objeto reserva criado
+     */
+
+    @Override
+    public Reserva criar(Reserva obj) {
+        this.listaReserva.add(obj);
+        ArmazenamentoArquivo.guardar(this.listaReserva,this.nomeArquivo,this.nomePasta);
+
+        return obj;
+    }
+
+    /**
+     * Método que remove todas as reservas ativas de um leitor da estrutura de armazenamento (lista e arquivo).
+     *
+     * Esta forma de remoção em sequência, evita problemas de deslocamento de índice que podem
+     * ocorrer ao remover elementos de um ArrayList enquanto o percorremos.
+     *
+     * @param id identificação do leitor
+     */
+
+    @Override
+    public void remover(int id) {
+        List<Reserva> reservasARemover = new ArrayList<Reserva>();
+
+        for (Reserva reserva : this.listaReserva) {
+            if (reserva.getLeitor() == id)
+                reservasARemover.add(reserva);
+        }
+
+        this.listaReserva.removeAll(reservasARemover);
+        ArmazenamentoArquivo.guardar(this.listaReserva,this.nomeArquivo,this.nomePasta);
+    }
+
+    /**
+     * Deleta todos os objetos do tipo Reserva do banco de dados.
+     */
+
+    @Override
+    public void removerTodos() {
+        this.listaReserva.clear();
+        ArmazenamentoArquivo.guardar(this.listaReserva,this.nomeArquivo,this.nomePasta);
+    }
+
+    /**
+     * Método de retorno da Reserva através da busca por ID.
+     *
+     * @param id identificação do leitor que fez a reserva a ser encontrada
+     * @return retorna objeto reserva encontrado
+     */
+
+    @Override
+    public Reserva encontrarPorId(int id) {
+        for(Reserva reserva : this.listaReserva){
+            if(reserva.getLeitor() == id){
+                return reserva;
+            }
+        }
+
+        return null;
+    }
+
+    /**
+     * Método que encontra todas as reservas ativas.
+     *
+     * @return retorna lista de reservas ativas encontradas
+     */
+
+    @Override
+    public List<Reserva> encontrarTodos() {
+        return this.listaReserva;
+    }
+
+    /**
+     * Método que encontra todas as reserva ativas de um livro.
+     *
+     * Objetos Reserva que possuam o ISBN informado, são adicionados numa
+     * lista e retornados.
+     *
+     * @param isbn isbn do livro reservado
+     * @return retorna lista de reservas ativas de um livro
+     */
+
+    @Override
+    public List<Reserva> encontrarReservasLivro(double isbn) {
+        List<Reserva> listaReservaLeitor = new ArrayList<Reserva>();
+
+        for(Reserva reserva : this.listaReserva){
+            if(reserva.getLivro() == isbn)
+                listaReservaLeitor.add(reserva);
+        }
+
+        return listaReservaLeitor;
+    }
+
+    /**
+     * Método que encontra todas as reserva ativas de um leitor.
+     *
+     * Objetos Reserva que possuam o ID informado, são adicionados numa
+     * lista e retornados.
+     *
+     * @param id identificação do leitor
+     * @return retorna lista de reservas ativas de um leitor
+     */
+
+    @Override
+    public List<Reserva> encontrarReservasLeitor(int id) {
+        List<Reserva> listaReservaLivro = new ArrayList<Reserva>();
+
+        for(Reserva reserva : this.listaReserva){
+            if(reserva.getLeitor() == id)
+                listaReservaLivro.add(reserva);
+        }
+
+        return listaReservaLivro;
+    }
+
+    /**
+     * Método que remove todas as reservas ativas de um livro da estrutura de armazenamento (lista e arquivo).
+     *
+     * Esta forma de remoção em sequência, evita problemas de deslocamento de índice que podem
+     * ocorrer ao remover elementos de um ArrayList enquanto o percorremos.
+     *
+     * @param isbn isbn do livro
+     */
+
+    @Override
+    public void removerReservasDeUmLivro(double isbn) {
+        List<Reserva> reservasARemover = new ArrayList<Reserva>();
+
+        for (Reserva reserva : this.listaReserva) {
+            if (reserva.getLivro() == isbn)
+                reservasARemover.add(reserva);
+        }
+
+        this.listaReserva.removeAll(reservasARemover);
+        ArmazenamentoArquivo.guardar(this.listaReserva,this.nomeArquivo,this.nomePasta);
+    }
+
+    /**
+     * Método que remove uma determinada reserva ativa da estrutura de armazenamento (lista e arquivo).
+     *
+     * @param id identificação do leitor
+     * @param isbn isbn do livro
+     */
+
+    @Override
+    public void removerUmaReserva(int id, double isbn) {
+        for(Reserva reserva : this.listaReserva){
+            if(reserva.getLeitor() == id && reserva.getLivro() == isbn) {
+                this.listaReserva.remove(reserva);
+                ArmazenamentoArquivo.guardar(this.listaReserva,this.nomeArquivo,this.nomePasta);
+                return;
+            }
+        }
+    }
+
+    /**
+     * Método que remove a primeira reserva de um livro da estrutura de armazenamento (lista e arquivo).
+     *
+     * @param isbn isbn do livro
+     */
+
+    @Override
+    public void removeTop1(double isbn) {
+        for(Reserva reserva : this.listaReserva){
+            if(reserva.getLivro() == isbn){
+                this.listaReserva.remove(reserva);
+                ArmazenamentoArquivo.guardar(this.listaReserva,this.nomeArquivo,this.nomePasta);
+                return;
+            }
+        }
+    }
+
+    /**
+     * Método que verifica se determinado livro possui alguma reserva ativa.
+     *
+     * @param isbn isbn do livro
+     * @return retorna true caso encontre e false caso não encontre
+     */
+
+    @Override
+    public boolean livroTemReserva(double isbn) {
+        for(Reserva reserva : this.listaReserva){
+            if(reserva.getLivro() == isbn)
+                return true;
+        }
+
+        return false;
+    }
+
+    /**
+     * Método que verifica se já possui uma reserva ativa de um livro por um leitor.
+     *
+     * @param id identificação do leitor
+     * @param isbn isbn do livro
+     * @return retorna true caso encontre e false caso não encontre
+     */
+
+    @Override
+    public boolean leitorJaReservouEsseLivro(int id, double isbn) {
+        for(Reserva reserva : this.listaReserva){
+            if(reserva.getLeitor() == id && reserva.getLivro() == isbn)
+                return true;
+        }
+
+        return false;
+    }
+
+    /**
+     * Método que encontra a primeira reserva de um livro.
+     *
+     * @param isbn isbn do livro
+     * @return retorna a primeira reserva encontrada de um livro
+     */
+
+    @Override
+    public Reserva top1Reserva(double isbn) {
+        for(Reserva reserva : this.listaReserva){
+            if(reserva.getLivro() == isbn)
+                return reserva;
+        }
+
+        return null;
+    }
+
+    @Override
+    public Reserva atualizar(Reserva obj) {
+        return null;
+    }
+}
