50dc52db32cc4ff5a0e918d3b65348dbcc56bad9
Lucas Gabriel <lucasfacul20222@gmail.com>
2023-09-28 17:05:10 -0300

    test: Adicionado testes das classes DAO Arquivo de Bibliotecario, Leitor, Livro, Emprestimo, Prazos e Reserva

---
+++ src/testes/dao/leitor/LeitorImplArquivoTest.java
@@ -0,0 +1,122 @@
+package testes.dao.leitor;
+
+import dao.DAO;
+import model.Leitor;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import utils.ArmazenamentoArquivo;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LeitorImplArquivoTest {
+
+    private Leitor leitor1, leitor2, leitor3, leitor4, leitor5;
+
+    @BeforeEach
+    void setUp() {
+        this.leitor1 = DAO.getLeitor().criar(new Leitor("Lucas", "Rua Alameda", "75757575", "batata"));
+        this.leitor2 = DAO.getLeitor().criar(new Leitor("Ana", "Avenida Central", "12345678", "senha123"));
+        this.leitor3 = DAO.getLeitor().criar(new Leitor("Pedro", "Rua das Flores", "98765432", "abcd1234"));
+        this.leitor4 = DAO.getLeitor().criar(new Leitor("Mariana", "Avenida Principal", "55555555", "senhamariana"));
+        this.leitor5 = DAO.getLeitor().criar(new Leitor("Carlos", "Rua dos Passarinhos", "11111111", "senha12345"));
+    }
+
+    @AfterEach
+    void tearDown() {
+        DAO.getLeitor().removerTodos();
+    }
+
+    @Test
+    void criar() {
+        Leitor esperado = new Leitor("Silas", "Rua México","7987675436","eletricidade");
+        esperado.setID(20000);
+        Leitor atual =  DAO.getLeitor().criar(esperado);
+
+        assertEquals(esperado, atual);
+    }
+
+    @Test
+    void remover() {
+        assertNotNull(DAO.getLeitor().encontrarPorId(1013));
+        assertNotNull(DAO.getLeitor().encontrarPorId(1023));
+
+        DAO.getLeitor().remover(1013);
+        DAO.getLeitor().remover(1023);
+
+        assertNull(DAO.getLeitor().encontrarPorId(1013));
+        assertNull(DAO.getLeitor().encontrarPorId(1023));
+
+        assertEquals(3,DAO.getLeitor().encontrarTodos().size());
+        assertEquals(3,ArmazenamentoArquivo.resgatar("leitor.dat","Leitor").size());
+    }
+
+    @Test
+    void removerTodos() {
+        DAO.getLeitor().removerTodos();
+
+        assertTrue(DAO.getLeitor().encontrarTodos().isEmpty());
+        assertTrue(ArmazenamentoArquivo.resgatar("leitor.dat","Leitor").isEmpty());
+        assertEquals(1003,DAO.getLeitor().criar(new Leitor("novoLeitor","rua","444","senhaNova")).getID());
+    }
+
+    @Test
+    void encontrarPorId() {
+        Leitor esperado = this.leitor3;
+        Leitor atual = DAO.getLeitor().encontrarPorId(1023);
+
+        assertEquals(esperado, atual);
+    }
+
+    @Test
+    void encontrarTodos() {
+        assertEquals(5, DAO.getLeitor().encontrarTodos().size());
+        assertEquals(this.leitor1, DAO.getLeitor().encontrarTodos().get(0));
+        assertEquals(this.leitor2, DAO.getLeitor().encontrarTodos().get(1));
+        assertEquals(this.leitor3, DAO.getLeitor().encontrarTodos().get(2));
+        assertEquals(this.leitor4, DAO.getLeitor().encontrarTodos().get(3));
+        assertEquals(this.leitor5, DAO.getLeitor().encontrarTodos().get(4));
+        assertEquals(5,ArmazenamentoArquivo.resgatar("leitor.dat","Leitor").size());
+    }
+
+    @Test
+    void atualizar() {
+        this.leitor1.setSenha("novasenha");
+        this.leitor1.setNome("joaquim");
+        this.leitor1.setDataMulta(LocalDate.now());
+        Leitor esperado = this.leitor1;
+
+        Leitor atual = DAO.getLeitor().atualizar(this.leitor1);
+
+        assertEquals(esperado,atual);
+    }
+
+    @Test
+    void encontrarPorNome() {
+        Leitor esperado = this.leitor2;
+        Leitor esperado2 = DAO.getLeitor().criar(new Leitor("ANA","Rua Portugal","7665653434","tortuguita"));
+
+        List<Leitor> atual = DAO.getLeitor().encontrarPorNome("ANA");
+
+        assertEquals(2,atual.size());
+
+        assertEquals(esperado, atual.get(0));
+        assertEquals(esperado2, atual.get(1));
+    }
+
+    @Test
+    void encontrarPorTelefone() {
+        Leitor esperado = this.leitor5;
+        Leitor esperado2 = DAO.getLeitor().criar(new Leitor("pedro","Rua Portugal","11111111","tortuguita"));
+
+        List<Leitor> atual = DAO.getLeitor().encontrarPorTelefone("11111111");
+
+        assertEquals(2,atual.size());
+
+        assertEquals(esperado, atual.get(0));
+        assertEquals(esperado2, atual.get(1));
+    }
+}
---
+++ src/testes/dao/livro/LivroImplArquivoTest.java
@@ -0,0 +1,117 @@
+package testes.dao.livro;
+
+import dao.DAO;
+import model.Livro;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import utils.ArmazenamentoArquivo;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class LivroImplArquivoTest {
+
+    private Livro livro1, livro2, livro3, livro4, livro5;
+
+    @BeforeEach
+    void setUp() {
+        this.livro1 = DAO.getLivro().criar(new Livro("LIVRO1","AUTOR1","EDITORA1",2001,"CATEGORIA1"));
+        this.livro2 = DAO.getLivro().criar(new Livro("LIVRO2","AUTOR2","EDITORA2",2002,"CATEGORIA2"));
+        this.livro3 = DAO.getLivro().criar(new Livro("LIVRO3","AUTOR3","EDITORA3",2003,"CATEGORIA3"));
+        this.livro4 = DAO.getLivro().criar(new Livro("LIVRO4","AUTOR4","EDITORA4",2004,"CATEGORIA4"));
+        this.livro5 = DAO.getLivro().criar(new Livro("LIVRO5","AUTOR5","EDITORA5",2005,"CATEGORIA5"));
+    }
+
+    @AfterEach
+    void tearDown() {
+        DAO.getLivro().removerTodos();
+    }
+
+    @Test
+    void criar() {
+        Livro esperado = new Livro("As cronicas de eu","Xama","Viva mais",2002,"Terror");
+
+        Livro atual = DAO.getLivro().criar(esperado);
+        esperado.setISBN(atual.getISBN());
+
+        assertEquals(esperado, atual);
+    }
+
+    @Test
+    void removerPorISBN() {
+        Livro esperado = this.livro5;
+        double isbn5 = DAO.getLivro().encontrarPorTitulo("livro5").get(0).getISBN();
+
+        Livro atual = DAO.getLivro().encontrarPorISBN(isbn5);
+
+        assertEquals(esperado,atual);
+    }
+
+    @Test
+    void removerTodos() {
+        DAO.getLivro().removerTodos();
+
+        assertTrue(DAO.getLivro().encontrarTodos().isEmpty());
+        assertTrue(ArmazenamentoArquivo.resgatar("livro.dat","Livro").isEmpty());
+
+        //apagou todos os isbn, sorteou um e adicionou na lista
+        assertEquals(1,DAO.getLivro().checarListaISBN().size());
+
+        DAO.getLivro().criar(new Livro("A origem","Ana","Seu medo",1994,"Suspense"));
+        assertEquals(2,DAO.getLivro().checarListaISBN().size());
+    }
+
+    @Test
+    void encontrarTodos() {
+        assertEquals(5, DAO.getLivro().encontrarTodos().size());
+        assertEquals(5, ArmazenamentoArquivo.resgatar("livro.dat","Livro").size());
+    }
+
+    @Test
+    void atualizar() {
+        this.livro4.setCategoria("TERROR");
+        this.livro4.setAutor("EU MEMO");
+
+        Livro esperado = this.livro4;
+
+        Livro atual = DAO.getLivro().atualizar(this.livro4);
+
+        assertEquals(esperado,atual);
+    }
+
+    @Test
+    void encontrarPorAutor() {
+        DAO.getLivro().criar(new Livro("LIVRO1.2","AUTOR1","EDITORA1.2",2001,"CATEGORIA1.2"));
+
+        List<Livro> atual = DAO.getLivro().encontrarPorAutor("autor1");
+
+        assertEquals(2,atual.size());
+    }
+
+    @Test
+    void encontrarPorCategoria() {
+        DAO.getLivro().criar(new Livro("LIVRO2.1","AUTOR2.1","EDITORA2.1",2002,"CATEGORIA2"));
+
+        List<Livro> atual = DAO.getLivro().encontrarPorCategoria("categoria2");
+
+        assertEquals(2,atual.size());
+    }
+
+    @Test
+    void encontrarPorISBN() {
+        Livro esperado = this.livro5;
+        double isbn5 = DAO.getLivro().encontrarPorTitulo("livro5").get(0).getISBN();
+
+        Livro atual = DAO.getLivro().encontrarPorISBN(isbn5);
+
+        assertEquals(esperado,atual);
+    }
+
+    @Test
+    void checarListaISBN() {
+        assertEquals(6,DAO.getLivro().checarListaISBN().size());
+    }
+}
