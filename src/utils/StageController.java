b17432f75d6074ef2ce8505f474ab28432381ae3
Lucas Gabriel <lucasfacul20222@gmail.com>
2023-10-16 13:59:30 -0300

    feat: Adicionado método para criar Stage e retornar objeto FXMLLoader
    Este m´todo serve para setar uma scene num stage. Recebe como parametro o stage a ser setado e o nome da "scene" que irá compor o stage.

--- src/utils/StageController.java
+++ src/utils/StageController.java
@@ -1,10 +1,26 @@
 package utils;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
+import view.MainApplication;
+
+import java.io.IOException;
 
 public class StageController {
+
+    public static FXMLLoader criaStage(Stage stage,String nomeTela) throws IOException {
+        FXMLLoader loader = new FXMLLoader(MainApplication.class.getResource(nomeTela));
+        Scene scene = new Scene(loader.load());
+        stage.setScene(scene);
+        stage.setResizable(false);
+        stage.setTitle("Sistema de Gerenciamento de Biblioteca");
+        stage.centerOnScreen();
+        stage.show();
+        return loader;
+    }
 
     public static Stage getStage(ActionEvent event){
         Node source = (Node)  event.getSource();
