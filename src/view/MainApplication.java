1668e0fc5834002cd3d78690f0c70ce64127c8ef
Lucas Gabriel <lucasfacul20222@gmail.com>
2023-10-17 13:52:01 -0300

    feat: Alterações na tela de livros pesquisados

--- src/view/MainApplication.java
+++ src/view/MainApplication.java
@@ -20,8 +20,13 @@ public class MainApplication extends Application {
         //DAO.getLeitor().criar(new Leitor("leitor1","rua1","telefone1","senha3"));
         //DAO.getLivro().criar(new Livro("lucas","lucas","lucas",2018,"lucas"));
         //DAO.getLivro().criar(new Livro("lucas","lucas","lucas",2018,"lucas"));
+        //DAO.getEmprestimo().criar(new Emprestimo(10.0000,1013));
+        //DAO.getReserva().removerTodos();
         System.out.println(DAO.getLivro().encontrarTodos().get(0).getISBN());
         System.out.println(DAO.getLivro().encontrarTodos().get(1).getISBN());
+        System.out.println(DAO.getAdm().encontrarTodos().get(0).getID());
+        System.out.println(DAO.getBibliotecario().encontrarTodos().get(0).getID());
+        System.out.println(DAO.getLeitor().encontrarTodos().get(0).getID());
 
         StageController.criaStage(stage,"TelaLogin.fxml");
         stage.show();
